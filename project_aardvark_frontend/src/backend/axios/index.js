257018ecb382080b0f647bb6fc7ef695957b3f8e
Alex Henegar <alex.henegar19@gmail.com>
2020-02-17 15:53:40 -0500

    setup initial axios usage

---
+++ project_aardvark_frontend/src/backend/axios/index.js
@@ -0,0 +1,57 @@
+// import vue from "vue"
+import axios from "axios"
+// import VueAxios from "vue-axios";
+
+const API_URL = 'http://localhost:3000'
+
+const securedAxiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+const plainAxiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+securedAxiosInstance.interceptors.request.use(config => {
+  const method = config.method.toUpperCase()
+  if (method !== 'OPTIONS' && methods !== 'GET') {
+    config.headers = {
+      ...config.headers,
+      'X-CSRF-TOKEN': localStorage.csrf,
+    }
+  }
+  return config
+})
+
+securedAxiosInstance.interceptors.response.user(null, error => {
+  // cookie expired or 401 response
+  if (error.response && error.response.config && error.response.status === 401) {
+    return plainAxiosInstance.post('/refresh', {}, {headers: {'X-CSRF-TOKEN': localStorage.csrf}})
+      .then(response => {
+        localStorage.csrf = response.data.csrf
+        localStorage.signedIn = true
+
+        let retryConfig = error.response.config
+        retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
+        return plainAxiosInstance.request(retryConfig)
+      }).catch(error => {
+        delete localStorage.csrf
+        delete localStorage.signedIn
+
+        location.replace('/')
+        return Promise.reject(error)
+      })
+  } else {
+    return Promise.reject(error)
+  }
+})
+
+export {securedAxiosInstance, plainAxiosInstance}
